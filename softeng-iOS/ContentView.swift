a845c399e564d1d61c4c867f4ff012c83f5bffca
Alex <alexander.siracusa@gmail.com>
2024-03-31 09:14:32 -0400

    fixed assets loading

--- softeng-iOS/ContentView.swift
+++ softeng-iOS/ContentView.swift
@@ -6,25 +6,25 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                Image("00_thelowerlevel1.png")
-            }
-            .toolbar {
-                ToolbarItem {
-                    Button(action: addNodes) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
+        Image("00_thelowerlevel1")
+//        NavigationSplitView {
+//            List {
+//                Image("00_thelowerlevel1.png")
+//            }
+//            .toolbar {
+//                ToolbarItem {
+//                    Button(action: addNodes) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//        } detail: {
+//            Text("Select an item")
+//        }
     }
     
     private func addNodes() {
